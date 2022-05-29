28a9a481a82fa9c77df8510cb44df05952e7961d
Julian Castro <juliancastro@Julians-MacBook-Pro.local>
2022-05-29 15:35:01 -0400

    First init

---
+++ greeting-service/src/main/java/com/grpcex/grpc/GrpcSpringbootExApplication.java
@@ -0,0 +1,13 @@
+package com.grpcex.grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcSpringbootExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcSpringbootExApplication.class, args);
+	}
+
+}
---
+++ greeting-service/src/main/java/com/grpcex/grpc/service/GreetingServiceImpl.java
@@ -0,0 +1,21 @@
+package com.grpcex.grpc.service;
+
+import com.grpcex.grpc.*;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class GreetingServiceImpl extends GreetingServiceGr {
+    @Override
+    public void Greeting(GreetingRequest request, StreamObserver<GreetingResponse> responseObserver) {
+        String message = request.getMessage();
+        System.out.println("Received message: " + message);
+
+        GreetingResponse response = GreetingResponse.newBuilder()
+            .setMessage("Received your " + message + ". Hello From Server. ").build();
+        
+        // Send response to client
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
